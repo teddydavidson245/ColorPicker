3c633c8437429a3e1c90a25e80b4f08b036b8d11
Teddymutiga <teddydavidson245@gmail.com>
2023-07-30 17:25:15 -0500

    Added colors and different states.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 // Write your Color component here
+// const Color = (props) => {
+  
+//   return <div className={props.color}></div>
+// }
 
+const Color = ({color, setSelectedColor}) => {
+  return (
+    <div 
+      className={color} 
+      onClick={() => setSelectedColor(color)} 
+     ></div>
+  )
+}
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("")
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color = "green" setSelectedColor={setSelectedColor}/>
+        <Color color = "red" setSelectedColor={setSelectedColor}/>
+        <Color color = "black" setSelectedColor={setSelectedColor}/>
+      </div>
     </div>
   );
 };
